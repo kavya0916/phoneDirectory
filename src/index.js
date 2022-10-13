3ca64d8b00afc4bc0f5862732ce650a7fb8f9af8
Kavya hb <kavyahb21@gmail.com>
2022-10-13 18:37:31 +0530

    created phone directory application

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  // <div>Welcome to react sessions</div>
 );
 
 // If you want to start measuring performance in your app, pass a function
